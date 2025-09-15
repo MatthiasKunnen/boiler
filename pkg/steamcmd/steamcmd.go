a550f3f70884cf5b63d5a81675fc1c6f28c3ec9c
Matthias Kunnen <matthias.kunnen@gmail.com>
2025-09-15 18:18:55 +0200

    Implement steamcmd.Exec and LogOutUser

--- pkg/steamcmd/steamcmd.go
+++ pkg/steamcmd/steamcmd.go
@@ -1,8 +1,7 @@
 package steamcmd
 
 import (
-	"bufio"
-	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -10,17 +9,18 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Opts struct {
 	// If empty, anonymous will be used.
-	LoginUsername string
-	// If empty, will be prompted on stdin.
-	LoginPassword         string
+	LoginUsername         string
 	InstallDir            string
 	DownloadGames         []DownloadGameOpts
 	DownloadWorkshopItems []DownloadWorkshopItemOpts
-	SteamCmdPath          string
+	// If true, the logged-in user will be logged out at the end of the run.
+	Logout       bool
+	SteamCmdPath string
 }
 
 type DownloadGameOpts struct {
@@ -34,43 +34,10 @@ type DownloadWorkshopItemOpts struct {
 	WorkshopItemId uint64
 }
 
-var steamCmdGuardCodePrompt = []byte("Steam Guard code:")
-var steamCmdPasswordPrompt = []byte("password: ")
-var steamCmdReady = []byte{
-	// Steam> with some ansi escapes
-	0x1B, 0x5B, 0x30, 0x6D, 0x1B, 0x5B, 0x31, 0x6D, 0x0A, 0x53, 0x74, 0x65,
-	0x61, 0x6D, 0x3E, 0x1B, 0x5B,
-}
-var largestNoDelimPrompt int
-
-func init() {
-	var noDelimPrompts = [][]byte{
-		steamCmdGuardCodePrompt,
-		steamCmdPasswordPrompt,
-		steamCmdReady,
-	}
-	for _, prompt := range noDelimPrompts {
-		if len(prompt) > largestNoDelimPrompt {
-			largestNoDelimPrompt = len(prompt)
-		}
-	}
-}
-
-var steamCmdUserLoggedIn = []byte("Waiting for user info...OK")
-var steamApiLoaded = []byte{
-	0x4C, 0x6F, 0x61, 0x64, 0x69, 0x6E, 0x67, 0x20, 0x53, 0x74, 0x65, 0x61,
-	0x6D, 0x20, 0x41, 0x50, 0x49, 0x2E, 0x2E, 0x2E, 0x1B, 0x5B, 0x30, 0x6D,
-	0x4F, 0x4B, 0x0A,
-}
-
-type steamCmdState int
-
-const (
-	steamCmdWaitingForReady steamCmdState = iota
-	steamCmdWaitingFor
-)
-
-func Exec(opts Opts) error {
+// Exec creates a script for steamcmd to process, writes it to a temporary file, and then
+// executes it. The runscript is removed after execution.
+// Stdin, stdout, and stderr are connected to their respective stream.
+func Exec(ctx context.Context, opts Opts) error {
 	if opts.SteamCmdPath == "" {
 		return fmt.Errorf("SteamCmdPath not set")
 	}
@@ -86,13 +53,25 @@ func Exec(opts Opts) error {
 		return fmt.Errorf("error checking if InstallDir exists: %w", err)
 	}
 
-	commands := make([]string, 0, 4+len(opts.DownloadGames)+len(opts.DownloadWorkshopItems))
-	commands = append(
-		commands,
-		"@ShutdownOnFailedCommand 1",
-		"force_install_dir "+opts.InstallDir,
-		"login "+opts.LoginUsername,
-	)
+	temp, err := os.CreateTemp("", "steamcmd_script")
+	if err != nil {
+		return fmt.Errorf("error creating temp file: %w", err)
+	}
+	defer os.Remove(temp.Name())
+	defer temp.Close()
+
+	_, err = io.WriteString(temp, "@ShutdownOnFailedCommand 1\n")
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(temp, "force_install_dir "+opts.InstallDir+"\n")
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(temp, "login "+opts.LoginUsername+"\n")
+	if err != nil {
+		return err
+	}
 
 	var sb strings.Builder
 	for _, game := range opts.DownloadGames {
@@ -106,144 +85,89 @@ func Exec(opts Opts) error {
 		if game.Validate {
 			sb.WriteString(" validate")
 		}
-		commands = append(commands, sb.String())
+		sb.WriteString("\n")
+		_, err = io.WriteString(temp, sb.String())
+		if err != nil {
+			return err
+		}
 	}
 	for _, item := range opts.DownloadWorkshopItems {
-		commands = append(commands, fmt.Sprintf(
-			"download_item %d %d",
+		_, err = io.WriteString(temp, fmt.Sprintf(
+			"download_item %d %d\n",
 			item.GameId,
 			item.WorkshopItemId,
 		))
-	}
-	commands = append(commands, "quit")
-	commandsIndex := 0
-
-	cmd := exec.Command(opts.SteamCmdPath)
-
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return fmt.Errorf("could not get stdin pipe: %w", err)
+		if err != nil {
+			return err
+		}
 	}
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("could not get stdout pipe: %w", err)
+	if opts.Logout {
+		_, err = io.WriteString(temp, "logout\n")
+		if err != nil {
+			return err
+		}
 	}
 
-	f, err := os.Create("steamcmd.log")
+	_, err = io.WriteString(temp, "quit\n")
 	if err != nil {
 		return err
 	}
-	stdout = io.NopCloser(io.TeeReader(stdout, f))
 
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return fmt.Errorf("could not get stderr pipe: %w", err)
-	}
+	cmd := exec.CommandContext(
+		ctx,
+		opts.SteamCmdPath,
+		"+runscript",
+		temp.Name(),
+	)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	err = cmd.Start()
 	if err != nil {
-		return err
+		return fmt.Errorf("error starting steamcmd: %w", err)
 	}
 
-	r := bufio.NewReaderSize(stdout, 4096)
-	osStdin := bufio.NewReader(os.Stdin)
-	for {
-		peekBytes, err := r.Peek(largestNoDelimPrompt)
-		log.Printf("Peeked %x", peekBytes)
-		if err != nil && err != io.EOF {
-			log.Println("Error peeking:", err)
-			break
-		}
-
-		if bytes.HasPrefix(peekBytes, steamCmdPasswordPrompt) {
-			log.Printf("steamcmd: %s", peekBytes)
-			_, err := r.Discard(len(steamCmdPasswordPrompt))
-			if err != nil {
-				return err
-			}
-
-			var password string
-			if opts.LoginPassword != "" {
-				password = opts.LoginPassword
-			} else {
-				password, err = osStdin.ReadString('\n')
-				if err != nil {
-					return fmt.Errorf("error reading password from stdin: %w", err)
-				}
-			}
-
-			err = writeWithNewline(stdin, password)
-			if err != nil {
-				return err
-			}
-		} else if bytes.HasPrefix(peekBytes, steamCmdGuardCodePrompt) {
-			log.Printf("steamcmd: %s", peekBytes)
-			_, err := r.Discard(len(steamCmdGuardCodePrompt))
-			if err != nil {
-				return err
-			}
-			code, err := osStdin.ReadString('\n')
-			if err != nil {
-				return fmt.Errorf("error reading steam guard code from stdin: %w", err)
-			}
-			err = writeWithNewline(stdin, code)
-			if err != nil {
-				return err
-			}
-		} else if bytes.HasPrefix(peekBytes, steamCmdReady) {
-			_, err := r.Discard(len(steamCmdReady))
-			if err != nil {
-				return err
-			}
-			if commandsIndex >= len(commands) {
-				// quit
-				break
-			}
-			command := commands[commandsIndex]
-			err = writeWithNewline(stdin, command)
-			if err != nil {
-				return fmt.Errorf("failed to send command %s: %w", command, err)
-			}
-			commandsIndex++
-		} else {
-			line, err := r.ReadBytes('\n')
-			if err != nil {
-				if err == io.EOF {
-					// End of output
-					break
-				}
-				log.Println("Error reading line:", err)
-				break
-			}
-			log.Printf("steamcmd: %s", line)
-			log.Printf("steamcmd hex: %x", line)
-		}
+	steamCmdErr := cmd.Wait()
+	if steamCmdErr == nil || !opts.Logout || opts.LoginUsername == "anonymous" {
+		return steamCmdErr
 	}
 
-	go func() {
-		s := bufio.NewScanner(stderr)
-		for s.Scan() {
-			log.Printf("steamcmd stderr: %s\n", s.Text())
-		}
-		if err := s.Err(); err != nil {
-			log.Printf("steamcmd error processing stderr: %v\n", err)
-		}
-	}()
+	log.Print("An error occurred and logout may not have occurred, attempting to logout...")
+	logoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	err = LogOutUser(logoutCtx, opts.SteamCmdPath, opts.LoginUsername)
+	if err == nil {
+		log.Print("Logout successful")
+	} else {
+		log.Printf("WARNING: steamcmd exited unexpectedly and attempting logout failed. Your credentials may still be stored on the system. %v", err)
+	}
 
-	return cmd.Wait()
+	return steamCmdErr
 }
 
-func writeWithNewline(w io.Writer, content string) error {
-	log.Printf("writing: %s", content)
-	_, err := io.WriteString(w, content)
+func LogOutUser(ctx context.Context, steamCmdPath string, username string) error {
+	logoutTmp, err := os.CreateTemp("", "steamcmd_logout")
+	if err != nil {
+		return fmt.Errorf("error creating temp file: %w", err)
+	}
+	defer os.Remove(logoutTmp.Name())
+	defer logoutTmp.Close()
+	_, err = fmt.Fprintf(logoutTmp, `@ShutdownOnFailedCommand 1
+login %s
+logout
+quit
+`, username)
 	if err != nil {
 		return err
 	}
-	_, err = w.Write([]byte("\n"))
-	if err != nil {
-		return fmt.Errorf("error writing newline: %w", err)
-	}
 
-	return nil
+	logoutCmd := exec.CommandContext(
+		ctx,
+		steamCmdPath,
+		"+runscript",
+		logoutTmp.Name(),
+	)
+	return logoutCmd.Run()
 }
